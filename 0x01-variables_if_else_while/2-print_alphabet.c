53dbc234d5a15d09c499d6343d5d1e39d3aff349
“Brigg-commit” <ibridget95@gmail.com>
2022-07-01 14:49:14 +0100

    Aprogram that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/** main - prints the alphabet in lowercase, followed* by a new line.
+*Return: returns zero if program exits well.
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
