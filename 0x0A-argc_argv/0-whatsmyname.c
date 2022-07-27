de64fae1a90fa866a15182610203b6c58878c73e
“Brigg-commit” <ibridget95@gmail.com>
2022-07-28 00:32:19 +0100

    0-whatsmyname.c file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,9 @@
 /**
  * main - printf program name
  * @argc: no of arguments
- * @argv: array of strng parameters
+ * @argv: array of string parameters
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
   printf("%s\n", argv[argc - 1]);
