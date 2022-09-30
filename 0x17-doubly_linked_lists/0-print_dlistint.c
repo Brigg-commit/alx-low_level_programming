aae44ccb95927d9257e1e73b6c26bd1bbd97a2b1
“Brigg-commit” <ibridget95@gmail.com>
2022-09-30 17:15:56 +0100

    commit task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,30 +1,20 @@
-#include "lists.h"
-
-/**
- * print_dlistint - prints all the elements of a
- * dlistint_t list
- *
- * @h: head of the list
- * Return: the number of nodes
- */
-size_t print_dlistint(const dlistint_t *h)
-{
-	int counter;
-
-	counter = 0;
-
-	if (h == NULL)
-		return (counter);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		counter++;
-		h = h->next;
-	}
-
-	return (counter);
-}
+#include "lists.h" 
+#include <stdio.h> 
+  
+ /** 
+  *   * print_dlistint - print all the elements of a dlistint_t list 
+  *    * @h: pointer to the start of the linked list 
+  *     * 
+  *      * Return: number of nodes 
+  *       */ 
+ size_t print_dlistint(const dlistint_t *h) 
+	 { 
+		          size_t i; 
+		    
+		           for (i = 0; h != NULL; i++) 
+			            { 
+				                     printf("%d\n", h->n); 
+				                      h = h->next; 
+				               } 
+		            return (i); 
+		     }
