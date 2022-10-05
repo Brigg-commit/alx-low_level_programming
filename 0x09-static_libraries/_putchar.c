9c08b2e65067bdaf758ef2e14d52350cdfccfa95
“Brigg-commit” <ibridget95@gmail.com>
2022-10-05 06:27:31 +0100

    commit putchar file

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,28 +1,13 @@
-#include <unistd.h>
-
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 06a7c2f6ce272f0fc3c42f2810d349f0cae33b98
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-<<<<<<< HEAD
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-=======
-
-int _putchar(char c)
-
-{
-
-return (write(1, &c, 1));
-
->>>>>>> 06a7c2f6ce272f0fc3c42f2810d349f0cae33b98
-}
+#include <unistd.h> 
+  
+ /** 
+  * _putchar - writes the character c to stdout 
+  * @c: The character to print 
+  * 
+  * Return: On success 1. 
+  * On error, -1 is returned, and errno is set appropriately. 
+  */ 
+ int _putchar(char c) 
+ { 
+         return (write(1, &c, 1));
+ }
