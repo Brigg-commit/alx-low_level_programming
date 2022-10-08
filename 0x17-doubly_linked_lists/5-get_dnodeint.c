20662ad61442a94cfaa30afd3c82e003e0c753d8
“Brigg-commit” <ibridget95@gmail.com>
2022-10-08 22:56:32 +0100

    commit all

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,22 +10,12 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
-
-	while (head->prev != NULL)
-		head = head->prev;
-
-	i = 0;
-
-	while (head != NULL)
+	for (; index != 0; index--)
 	{
-		if (i == index)
-			break;
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
-		i++;
 	}
 
 	return (head);
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,9 @@
-#ifndef LIST_H 
-#define LIST_H 
-  
- #include <stddef.h> 
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
  /** 
   * struct dlistint_s - doubly linked list 
   * @n: integer 
@@ -15,7 +17,8 @@
  { 
      int n; 
      struct dlistint_s *prev; 
-     struct dlistint_s *next; 
+     struct dlistint_s *next;
+     size_t n;
  } dlistint_t; 
   
  size_t print_dlistint(const dlistint_t *h); 
@@ -28,4 +31,4 @@
  dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n); 
  int delete_dnodeint_at_index(dlistint_t **head, unsigned int index); 
   
-#endif/*LIST_H*/
+#endif /*LISTS_H*/
